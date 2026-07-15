38685b4a3a373969f7b2426e4f05efed5bd32cc7
PCGIT-895 <katymanuel03@gmail.com>
2026-07-15 17:40:30 -0600

    Delete plan-rg-only.ps1

--- plan-rg-only.ps1
+++
@@ -1,8 +0,0 @@
-# Test plan limited to only the 4 dev Resource Group modules.
-# Run from inside the landing-zone folder: .\plan-rg-only.ps1
-terraform plan `
-  -var-file="environments/dev.tfvars" `
-  -target=module.rg_network_primary `
-  -target=module.rg_security_primary `
-  -target=module.rg_monitoring_primary `
-  -target=module.rg_backup_primary
